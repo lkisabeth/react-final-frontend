49384f5435def7ac957e91271719db33bb7087e7
lukeymac <lukey.mac@outlook.com>
2017-08-30 14:06:02 -0400

    build constructor function for StickyNoteForm

--- src/components/StickyNoteForm.js
+++ src/components/StickyNoteForm.js
@@ -2,5 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class StickyNoteForm extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: this.props.stickynote.title,
+      body: this.props.stickynote.body
+    }
+  }
 }
