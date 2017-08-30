dbe677f8c542f9d027bd48eaf57d5160de22d778
lukeymac <lukey.mac@outlook.com>
2017-08-30 12:53:11 -0400

    stub out stickynotescontainer.js

---
+++ src/components/StickyNotesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class StickyNotesContainer extends Component {
+  render() {
+    return (
+      <div className="">
+        StickyNotes
+      </div>
+    );
+  }
+}
