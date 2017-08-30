4c5c84a1c69caecf2b2ae1e1acc66c9b23a4424a
lukeymac <lukey.mac@outlook.com>
2017-08-30 12:55:57 -0400

    add constructor to stickynotescontainer.js

--- src/components/StickyNotesContainer.js
+++ src/components/StickyNotesContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 export default class StickyNotesContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stickynotes: []
+    }
+  }
+
   render() {
     return (
       <div className="">
