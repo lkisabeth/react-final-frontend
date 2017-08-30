0577ce659d66f86c1c26e43202003bbd222f164e
lukeymac <lukey.mac@outlook.com>
2017-08-30 13:13:39 -0400

    add componentDidUpdate

--- src/components/StickyNotesContainer.js
+++ src/components/StickyNotesContainer.js
@@ -9,6 +9,14 @@ export default class StickyNotesContainer extends Component {
     }
   }
 
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/sticky_notes.json')
+    .then(response => {
+      this.setState({stickynotes: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="">
