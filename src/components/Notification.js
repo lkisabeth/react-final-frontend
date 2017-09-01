0797d9d0c8150e2300b2fc64c9d40a10793b1256
lukeymac <lukey.mac@outlook.com>
2017-08-31 21:52:58 -0400

    building Notification component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,1 +1,19 @@
 import React from 'react'
+import Transition from 'react-transition-group/Transition'
+
+const Notification = ({ in: inProp, notification }) =>
+  <Transition in={inProp} timeout={duration}>
+    {(transitionState) => {
+      return(
+        <span style={{
+          ...defaultStyle,
+          ...transitionStyles[transitionState]
+        }}>
+          {notification}
+        </span>
+      )
+
+    }}
+  </Transition>
+
+export default Notification
